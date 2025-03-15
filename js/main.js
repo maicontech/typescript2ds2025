36b59fc580453715f42857ce60025048e97ac916
maicontech <maicon.ribeiro13@etec.sp.gov.br>
2025-03-14 22:37:08 -0300

    criando classe cliente

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ var empresa;
     const calc = document.getElementById("calc");
     const campoNome = document.getElementById("campoNome");
     const campoAno = document.getElementById("campoAno");
+    const campoSaldo = document.getElementById("saldo");
     calc.addEventListener("click", () => {
-        let p = new empresa.Pessoa();
+        let p = new empresa.Cliente(90);
         p.nome = campoNome.value;
         p.anoNasc = parseInt(campoAno.value);
+        // p.codigo = 90
+        p.saldo = 100;
         document.getElementById("nome").textContent = p.nome;
         document.getElementById("ano").textContent = p.anoNasc.toString();
         document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
+        document.getElementById("codigo").textContent = p.codigo.toString();
+        document.getElementById("saldo").textContent = p.saldo.toString();
     });
 })(empresa || (empresa = {}));
